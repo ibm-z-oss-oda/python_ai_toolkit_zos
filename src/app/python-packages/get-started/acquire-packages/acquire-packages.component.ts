eb223000cd75799c204663db0852f1657b289760
Evan Rivera <Evan.Rivera@ibm.com>
2023-02-23 22:13:41 -0800

    Get started updates

--- src/app/python-packages/get-started/acquire-packages/acquire-packages.component.ts
+++ src/app/python-packages/get-started/acquire-packages/acquire-packages.component.ts
@@ -8,14 +8,33 @@ import { MessageService } from '../../services/message.service';
   styleUrls: ['./acquire-packages.component.scss']
 })
 export class AcquirePackagesComponent implements OnInit {
-  installCommand: string = `pip install --no-deps -r requirements.txt`;
+  installCommand: string = `pip install --no-deps -r pyaitoolkit_cp311_20230224.txt`;
+  installCommand2: string = `pip install --no-deps -r my_pyyaml_requirements.txt`;
 
-  pyyamlCommand: string = `pip install pyyaml`;
+  pyyamlCommand: string = `
+  pyyaml==6.0 
+  --hash=sha256:96787f42a61916311549aacd47e4bca100092d51ab0f098cbb3f778fcdd241fc
+`;
 
   pyyamlCommand6: string = `pip install pyyaml==6.0`;
 
   py310url: string = "https://ibm.biz/BdPnfF";
   py311url: string = "https://ibm.biz/BdPnfE";
+
+  command: string = `
+  --index-url    https://downloads.pyaitoolkit.ibm.net:443/repository/python_ai_toolkit_zos/simple
+
+  --trusted-host downloads.pyaitoolkit.ibm.net 
+  
+  --require-hashes 
+  
+  --only-binary :all:
+  
+  pyyaml==6.0 
+     --hash=sha256:96787f42a61916311549aacd47e4bca100092d51ab0f098cbb3f778fcdd241fc`;
+
+  
+  
   
   constructor(public _messageService: MessageService, public headService: HeaderService) { }
 
--- src/app/python-packages/get-started/set-up-env/set-up-env.component.ts
+++ src/app/python-packages/get-started/set-up-env/set-up-env.component.ts
@@ -9,12 +9,19 @@ import { MessageService } from '../../services/message.service';
 export class SetUpEnvComponent implements OnInit {
 
   openSDK: string = "https://www.ibm.com/products/open-enterprise-python-zos";
+  reqURL: string = "https://github.com/ibm-z-oss-oda/python_ai_toolkit_zos/tree/main/requirements";
+  pipDocs: string = "https://pip.pypa.io/en/stable/topics/configuration/";
   
   command: string = `
   [global]
   index = https://downloads.pyaitoolkit.ibm.net:443/repository/python_al_toolkit_zos/
   index-url = https://downloads.pyaitoolkit.ibm.net:443/repository/python_ai_toolkit_zos/simple
   trusted-host = downloads.pyaitoolkit.ibm.net`;
+
+  command2: string = `
+  --index-url https://downloads.pyaitoolkit.ibm.net:443/repository/python_ai_toolkit_zos/simple
+
+  --trusted-host downloads.pyaitoolkit.ibm.net`;
   
   constructor(public _messageService: MessageService) { }
 
@@ -24,5 +31,12 @@ export class SetUpEnvComponent implements OnInit {
   launchOpenSDK() {
     window.open(this.openSDK);
   }
+  
+  launchReq() {
+    window.open(this.reqURL);
+  }
 
+  launchPipDocs() {
+    window.open(this.pipDocs);
+  }
 }
